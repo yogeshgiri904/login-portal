3edd6c993ff9fd19fdf185b0b2de190ca12b444c
Yogesh Giri <53947392+yogeshgiri904@users.noreply.github.com>
2021-02-11 23:40:26 +0530

    2

--- sessionOut.php
+++ sessionOut.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="refresh" content="5;URL=login.php">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>redirecting...</title>
 </head>
@@ -11,8 +12,24 @@
     session_start();
     session_unset();
     session_destroy();
-    echo "You have been logged out";
-    echo "<br>Redirecting to login page...";
+    echo "<h2>You have been logged out !!!</h2>";
+    echo '<br><br>Redirecting to <a href="login.php"> login </a> page';
+    echo '<div id="countdown">5 seconds remaining... </div>';
+
 ?>
+
+<script>
+var timeleft = 4;
+var downloadTimer = setInterval(function(){
+  if(timeleft > 0){
+    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining... ";
+  } 
+  else {
+    document.getElementById("countdown").innerHTML = "0";
+  }
+  timeleft -= 1;
+}, 1000);
+
+</script>
 </body>
 </html>
---
+++ update.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <title>Update Profile</title>
+</head>
+<body>
+    <form method="POST">
+    <div class="form-row">
+        <div class="form-group col-md-6">
+          <img src="" alt="">
+          <input type="text" name="name" class="form-control">
+        </div>
+    
+        <div class="form-group col-md-6">
+          <label>Mobile</label>
+          <input type="number" maxlength="10" name="mobile" class="form-control" >
+        </div>
+    
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label>Email</label>
+          <input type="email" name="email" class="form-control">
+        </div>
+    
+        <div class="form-group col-md-6">
+          <label>Password</label>
+          <input type="password" name="pass" class="form-control">
+        </div>
+      </div>
+    
+      </div>
+      </br>
+    <input type="submit" name="submit" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">
+  </form>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Do you want to save your Profile ?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
